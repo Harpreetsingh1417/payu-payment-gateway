d423226dbc4171b87f27ae088a4b9b27c9e8a714
Harpreet singh <hhharysingh1122@gmail.com>
2026-08-21 23:40:35 +0530

    ok

--- CSV_clean_with_python and Log error code extracted with python/mera folder/server.js
+++ CSV_clean_with_python and Log error code extracted with python/mera folder/server.js
@@ -63,3 +63,65 @@
 // print("Error Summary")
 // print("Total 4xx errors:", count_4xx)
 // print("Total 5xx errors:", count_5xx)
+
+
+
+
+// to print code count meaning
+// from http import HTTPStatus
+
+// log_file = "access_errors.log"
+
+// status_counts = {}
+
+// with open(log_file, "r") as file:
+//     for line in file:
+//         parts = line.split()
+
+//         try:
+//             status_code = int(parts[8])
+
+//             if 400 <= status_code <= 599:
+//                 status_counts[status_code] = status_counts.get(status_code, 0) + 1
+
+//         except (ValueError, IndexError):
+//             continue
+
+
+// print("Code\tCount\tMeaning")
+// print("-" * 45)
+
+// for code in sorted(status_counts):
+//     meaning = HTTPStatus(code).phrase
+//     print(f"{code}\t{status_counts[code]}\t{meaning}")
+
+
+
+
+
+
+
+
+
+
+
+// to print code and meaning
+// from http import HTTPStatus
+
+// log_file = "access_errors.log"
+
+// print("\nCode\tMeaning")
+
+// with open(log_file, "r") as file:
+//     for line in file:
+//         parts = line.split()
+
+//         try:
+//             status_code = int(parts[8])
+
+//             if 400 <= status_code <= 599:
+//                 meaning = HTTPStatus(status_code).phrase
+//                 print(f"\n{status_code} : {meaning}")
+
+//         except (ValueError, IndexError):
+//             continue
